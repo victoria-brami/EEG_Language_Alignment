15c1f6b436d1bf555240720d3eb590d8ba96c5be
victoria-brami <nepthune.enpc@gmail.com>
2023-10-16 12:16:25 +0100

    make some corrections due to missing contents in original repo

--- embeddings.py
+++ embeddings.py
@@ -8,61 +8,61 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 
 def get_embeddings(df, device):
-  words = df
+    words = df
 
-  marked_texts = []
+    marked_texts = []
 
-  for i in words:
-    marked_text = "[CLS] " + i + " [SEP]"
-    marked_texts.append(marked_text)
+    for i in words:
+        marked_text = "[CLS] " + i + " [SEP]"
+        marked_texts.append(marked_text)
 
-  tokenized = []
-  for i in marked_texts:
-    tokenized_text = tokenizer.tokenize(i)
-    tokenized.append(tokenized_text)
+    tokenized = []
+    for i in marked_texts:
+        tokenized_text = tokenizer.tokenize(i)
+        tokenized.append(tokenized_text)
 
-  index_token = []
+    index_token = []
 
-  for i in tokenized:
-    index_token.append(tokenizer.convert_tokens_to_ids(i))
+    for i in tokenized:
+        index_token.append(tokenizer.convert_tokens_to_ids(i))
 
-  segments = []
+    segments = []
 
-  for i in tokenized:
-    segments.append([1] * len(i))
+    for i in tokenized:
+        segments.append([1] * len(i))
 
-  tokens_tensors = []
-  for i in index_token:
-    tokens_tensor = torch.tensor([i])
-    tokens_tensors.append(tokens_tensor)
+    tokens_tensors = []
+    for i in index_token:
+        tokens_tensor = torch.tensor([i])
+        tokens_tensors.append(tokens_tensor)
 
-  segment_tensors = []
-  for i in segments:
-    segments_tensors = torch.tensor([i])
-    segment_tensors.append(segments_tensors)
+    segment_tensors = []
+    for i in segments:
+        segments_tensors = torch.tensor([i])
+        segment_tensors.append(segments_tensors)
 
-  model = BertModel.from_pretrained('bert-base-uncased',
-                                  output_hidden_states = True, 
-                                  ).to(device)
+    model = BertModel.from_pretrained('bert-base-uncased',
+                                    output_hidden_states = True, 
+                                    ).to(device)
 
-  model.eval()
+    model.eval()
 
-  output = []
-  hidden_state = []
-  for i in range(len(tokens_tensors)):
+    output = []
+    hidden_state = []
+    for i in range(len(tokens_tensors)):
 
-    with torch.no_grad():
+        with torch.no_grad():
 
-      outputs = model(tokens_tensors[i], segment_tensors[i])
-      output.append(outputs)
+        outputs = model(tokens_tensors[i], segment_tensors[i])
+        output.append(outputs)
 
-      hidden_states = outputs[2]
-      hidden_state.append(hidden_states)
+        hidden_states = outputs[2]
+        hidden_state.append(hidden_states)
 
-  embeddings = []
-  for i in range(len(hidden_state)):
-    token_vecs = hidden_state[i][-2][0]
-    embedding = torch.mean(token_vecs, dim=0)
-    embeddings.append(embedding)
+    embeddings = []
+    for i in range(len(hidden_state)):
+        token_vecs = hidden_state[i][-2][0]
+        embedding = torch.mean(token_vecs, dim=0)
+        embeddings.append(embedding)
 
-  return embeddings
+    return embeddings
